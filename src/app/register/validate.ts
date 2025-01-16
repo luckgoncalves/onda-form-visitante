ec3fca276307c9e8b616d550ab9b6e2a8fc86000
Lucas Gonçalves <lucasleitegoncalves@gmail.com>
2025-01-15 23:46:08 -0300

    fix: adjust validate format phone number and turn optional obervation

--- src/app/register/validate.ts
+++ src/app/register/validate.ts
@@ -11,15 +11,15 @@ export const step2Schema = z.object({
     idade: z.string({required_error: 'O valor informado nao é um número'}).min(1, { message: 'A idade é obrigatório' }),
     bairro: z.string({required_error: 'O bairro é obrigatório'}).min(1, { message: 'O bairro é obrigatório' }),
     estado_civil: z.string({required_error: 'Selecione um estado civil'}).min(1, { message: 'Selecione um estado civil' }),
-    telefone: z.string({required_error: 'O telefone é obrigatório'}).min(1, { message: 'O telefone é obrigatório' })
+    telefone: z.string({required_error: 'O telefone é obrigatório'}).refine(value => /^\d{2} \d{4,5}-\d{4}$/.test(value), { message: 'O telefone deve estar no formato 99 99999-9999 ou 99 9999-9999' })
 });
 
 export const step3Schema = z.object({
-    como_nos_conheceu: z.string({required_error: 'Campo obrigatório'}).min(1, { message: 'Campo obrigatório' }),
+    como_nos_conheceu: z.string().optional(),
     como_chegou_ate_nos: z.string().optional(),
     frequenta_igreja: z.string().optional(),
     qual_igreja: z.string().optional(),
     interesse_em_conhecer: z.array(z.string({required_error: 'Selecione pelo menos uma opção'}))
     .min(1, { message: 'Selecione pelo menos uma opção' }),
-    observacao: z.string({required_error: 'O campo é obrigatório'}).min(1, { message: 'O campo é obrigatório' }),
+    observacao: z.string().optional(),
 });
