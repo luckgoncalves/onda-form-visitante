b9721ef752b388fd0ae853444e0fbf2607666aa7
Lucas Gonçalves <lucasleitegoncalves@gmail.com>
2025-01-15 23:49:40 -0300

    fix: adjust validate format phone number

--- src/app/register/validate.ts
+++ src/app/register/validate.ts
@@ -11,7 +11,7 @@ export const step2Schema = z.object({
     idade: z.string({required_error: 'O valor informado nao é um número'}).min(1, { message: 'A idade é obrigatório' }),
     bairro: z.string({required_error: 'O bairro é obrigatório'}).min(1, { message: 'O bairro é obrigatório' }),
     estado_civil: z.string({required_error: 'Selecione um estado civil'}).min(1, { message: 'Selecione um estado civil' }),
-    telefone: z.string({required_error: 'O telefone é obrigatório'}).refine(value => /^\d{2} \d{4,5}-\d{4}$/.test(value), { message: 'O telefone deve estar no formato 99 99999-9999 ou 99 9999-9999' })
+    telefone: z.string({required_error: 'O telefone é obrigatório'}).refine(value => /^\(\d{2}\) \d{4,5}-\d{4}$/.test(value), { message: 'O telefone deve estar no formato (99) 99999-9999 ou (99) 9999-9999' })
 });
 
 export const step3Schema = z.object({
