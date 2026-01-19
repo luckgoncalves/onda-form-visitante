3b0894331c1b18092124f3d175db58160c69f02c
Lucas Gonçalves <lucasleitegoncalves@gmail.com>
2026-01-19 18:10:20 -0300

    ajuste style border textarea

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[80px] w-full  border border-b-black border-l-0 border-t-0 border-r-0 bg-transparent px-3 py-2 text-sm ring-0 placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-0 dark:placeholder:text-slate-400 dark:focus-visible:ring-0",
+          "flex min-h-[80px] w-full border rounded-md border-gray-300 focus:border-onda-darkBlue focus:ring-onda-darkBlue bg-transparent px-3 py-2 text-sm ring-0 placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-0 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-transparent dark:ring-0 dark:placeholder:text-slate-400 dark:focus-visible:ring-0",
           className
         )}
         ref={ref}
